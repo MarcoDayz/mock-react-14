1201bafbdb238efce51a1ef76a48ef14733e4404
MarcoDayz <diaz.marco8686@gmail.com>
2022-10-21 14:36:52 -0700

    updated

---
+++ src/components/PostBody.js
@@ -0,0 +1,14 @@
+const PostBody = (postBody) => {
+
+console.log(postBody)
+
+    return (
+        <>
+        <h1>
+            {postBody.body}
+        </h1>
+        </>
+    )
+}
+
+export default PostBody;
--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -1,6 +1,19 @@
-const PostItem = ({post}) => {
+const PostItem = ({post, setPostBody, handleClick}) => {
+
+    const getBody = async (e) => {
+        try {
+            const data = await fetch(`https://jsonplaceholder.typicode.com/posts/${post.id}`)
+            const json = await data.json();
+            setPostBody(json)
+            handleClick()
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+
+
     return(
-        <div className="listItem">
+        <div className="listItem" onClick={getBody}>
             <h1>{post.title}</h1>
             <p>{post.id}</p>
         </div>
